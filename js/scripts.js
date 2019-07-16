6e190433f29fbe936fdf8698250dec6203f9547c
Student <example@example.com>
2019-07-15 20:31:52 -0700

    added js to take input information and add it to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,25 @@
+// var user = {
+//   userName: $("input#userName").val(),
+//   userHeight: $("input#userHeight").val(),
+//   userWeight: $("input#userWeight").val(),
+//   userMeta: 0,
+// };
+
+
 $(document).ready(function() {
   $("#user-info").submit(function(event) {
-    
+    event.preventDefault();
+
+    var user = {
+      userName: $("input#userName").val(),
+      userHeight: $("input#userHeight").val(),
+      userWeight: $("input#userWeight").val(),
+      userMeta: 0,
+  };
+
+  $("#name").text(user.userName);
+  $("#height").text(user.userHeight);
+  $("#weight").text(user.userWeight);
+     console.log(user)
   })
 })
