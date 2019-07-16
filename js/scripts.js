049a98cc1c3f020fd75cee9173f2a3fefd926567
Student <example@example.com>
2019-07-15 20:19:47 -0700

    create user object

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,27 @@
+//Backend UI
+
+var user = {
+  userName: "user",
+  userHeight: 0,
+  userWeight: 0,
+  userMeta: 0,
+  userBac: 0,
+};
+
+var addDrink = function() {
+
+};
+
+
+var driveCheck = function() {
+
+};
+
+
+
+
 $(document).ready(function() {
   $("#user-info").submit(function(event) {
-    
+
   })
 })
