2ea2fb7b5ee8fc5867aefdc1a030e95cce3d0c02
Student <example@example.com>
2019-07-17 20:31:36 -0700

    updated form to take info for number of drinks and time since first drink, and than take that info and put it into the user variable.

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,6 @@ var user = {
   userFemale: true,
   userHeight: 0,
   userWeight: 0,
-  userMeta: 0,
   userBac: 0,
   userTime: 80,
   userDrinks: 0,
@@ -13,7 +12,12 @@ var user = {
 
 var drinkList = [];
 
+var gender = $("#gender").val();
+if (gender === "female") {
+  gender = true
+} else gender = false
 
+// var bmi = ((user.userWeight/(user.userHeight * 12)/(user.userHeight * 12) * 703)
 
 var addDrink = function() {
   updateBac();
@@ -35,7 +39,7 @@ var updateMeta = function() {
 
 };
 
-//This function checks if you are under the legal limit to driveCheck
+// This function checks if you are under the legal limit to driveCheck
 
 var driveCheck = function() {
   if (user.userBac <= .08) {
@@ -49,12 +53,14 @@ $(document).ready(function() {
     event.preventDefault();
 
       user.userName = $("input#new-first-name").val(),
+      user.userFemale = gender
       user.userHeight = $("input#new-last-name").val(),
       user.userWeight = $("input#new-weight").val(),
-      user.userMeta = 0,
       user.userBac = 0,
+      user.userTime = $("input#firstDrinkTime").val(),
+      user.userDrinks = $("input#numberDrinks").val(),
 
 
      console.log(user)
-  })
-})
+  });
+});
