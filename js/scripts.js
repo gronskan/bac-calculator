5eae6be410a36761b03bf3f90b459d3a7b223996
Student <example@example.com>
2019-07-22 19:43:24 -0700

    refine backend logic

--- js/scripts.js
+++ js/scripts.js
@@ -5,20 +5,15 @@ var user = {
   userFemale: true,
   userWeight: 0,
   userBac: 0,
-  userTime: 80,
+  userTime: 0,
   userDrinks: 0,
 };
 
-var gender = $("#gender").val();
-if (gender === "female") {
-  gender = true
-} else gender = false
-
-var updateTime = function() {
-  if (inputTime > user.userTime) {
-    user.userTime = inputTime;
+var updateUser = function() {
+  if (document.getElementById("radioMale").checked = true) {
+    user.userFemale = false;
+  } else {
   };
-  user.userBac = (user.userBac - ((user.userTime / 40 * 0.01)));
 };
 
 var updateBac = function() {
@@ -27,31 +22,32 @@ var updateBac = function() {
   } else {
     user.userBac = ((.06 - ((user.userWeight - 100)/2000) * user.userDrinks));
   };
+  user.userBac = (user.userBac - ((user.userTime / 40 * 0.01)));
 };
 
 // This function checks if you are under the legal limit to driveCheck
 
 var driveCheck = function() {
   if (user.userBac <= .08) {
-    return true
-  }
+    console.log("User CAN drive");
+    return true;
+  } else {
+    console.log("User CANNOT drive");
+  };
 };
 
 
 $(document).ready(function() {
-  $("#survey").submit(function(event) {
+  $("button#sub-button").click(function(event) {
     event.preventDefault();
 
-      user.userName = $("input#new-first-name").val(),
-      user.userFemale = gender
-      user.userHeight = $("input#new-last-name").val(),
-      user.userWeight = $("input#new-weight").val(),
-      user.userTime = $("input#firstDrinkTime").val(),
-      user.userDrinks = $("input#numberDrinks").val(),
-        
+      user.userName = $("input#userName").val(),
+      user.userWeight = $("input#userWeight").val(),
+      user.userTime = $("input#userTime").val(),
+      user.userDrinks = $("input#userDrinks").val(),
+      updateUser();
       updateBac();
-      updateTime();
       driveCheck();
       console.log("user is" + user);
-  })
-})
+  });
+});
