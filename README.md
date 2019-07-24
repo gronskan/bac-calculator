ee07cb0662e97362ae5e4cec7e0c0a128be7c9f1
Matt Eilar <meilar@gmail.com>
2019-07-24 11:48:47 -0700

    update BAC calculator logic following regression analysis

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Booze Calculator
 
 This is an educational project that uses California DMV guidelines to calculate whether the average person will be under the legal limits to operate a vehicle. This is intended to only be a guideline, please use this information responsibly.
+
+## Mathematical Operations
+
+Equation for determining BAC was obtained by running an exponential regression analysis on a chart from the data table provided by the California Department of Motor Vehicles (https://cadmv.wordpress.com/tag/blood-alcohol-content/). 
--- js/scripts.js
+++ js/scripts.js
@@ -17,17 +17,17 @@ var updateUser = function() {
 
 var updateBac = function() {
   if (user.userFemale) {
-    user.userBac = ((.08 - ((user.userWeight - 100)/2000) * user.userDrinks));
+    user.userBac = ((0.118358 * (0.993822**user.userWeight)) * user.userDrinks);
   } else {
-    user.userBac = ((.06 - ((user.userWeight - 100)/2000) * user.userDrinks));
+    user.userBac = ((0.115943 * (0.993084**user.userWeight)) * user.userDrinks);
   };
-  user.userBac = (user.userBac - ((user.userTime / 40 * 0.01)));
+    user.userBac = (user.userBac - ((user.userTime / 40 * 0.01)));
 };
 
 // This function checks if you are under the legal limit to driveCheck
 
 var driveCheck = function() {
-  if (user.userBac <= .08) {
+  if (user.userBac <= 0.08) {
     console.log("User CAN drive");
     return true;
   } else {
@@ -47,10 +47,10 @@ $(document).ready(function() {
       updateUser();
       updateBac();
       driveCheck();
-      if (updateBac <= .08) {
-        result = $("#notok").show();
-      } else result = $("#ok").show();
-      $("#bac").text(user.userBac)
+      if (driveCheck(true)) {
+        $("#ok").show();
+      } else $("#notok").show();
+      $(".bac").text((user.userBac).toFixed(2));
       $("#survey").hide();
   })
 })
