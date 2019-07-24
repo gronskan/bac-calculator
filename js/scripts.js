9f7fca5cf1a474d8f1efcf5cad07887d3af121fd
Matt Eilar <meilar@gmail.com>
2019-07-24 12:27:50 -0700

    remove grayscale UI template, rewrite tags to fit standard bootstrap

--- js/scripts.js
+++ js/scripts.js
@@ -40,6 +40,7 @@ var driveCheck = function() {
 $(document).ready(function() {
   $("button#sub-button").click(function(event) {
     event.preventDefault();
+    alert("WARNING: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.")
       user.userName = $("input#userName").val(),
       user.userWeight = $("input#userWeight").val(),
       user.userTime = $("input#userTime").val(),
